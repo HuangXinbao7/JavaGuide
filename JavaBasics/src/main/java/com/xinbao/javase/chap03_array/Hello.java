c24fc351e5e16912d76853a5a11993592fd4361c
Ming <3285119071@qq.com>
2020-12-15 09:21:26 +0800

    update

---
+++ JavaBasics/src/main/java/com/xinbao/javase/chap03_array/Hello.java
@@ -0,0 +1,23 @@
+package com.xinbao.javase.chap03_array;
+
+/**
+ * @Description 一个程序示例
+ * @author 作者
+ * @date 时间
+ */
+public class Hello {
+
+
+    /**
+     *
+     * @Description
+     * @author shkstart
+     * @date 2019年1月12日上午11:35:59
+     * @param n
+     * @return
+     */
+    public int getNumber(int n){
+        return 0;
+    }
+
+}
